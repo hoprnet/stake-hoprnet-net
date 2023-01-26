7b26f6d54f41d110cc5c694c09944777334a97a7
Michał Jadach <michal.jadach@hoprnet.org>
2023-01-26 20:11:46 +0100

    usefathom only on stake website

--- pages/_document.js
+++ pages/_document.js
@@ -10,7 +10,7 @@ class MyDocument extends Document {
             <link rel="preconnect" href="https://fonts.googleapis.com"/>
             <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true"/>
             <link href="https://fonts.googleapis.com/css2?family=Source+Code+Pro:ital,wght@0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,200;1,300;1,400;1,500;1,600;1,700;1,800&display=swap" rel="stylesheet"/>
-            <script src="https://cdn.usefathom.com/script.js" data-site="BDBWXXRL" defer></script>
+            <script src="https://cdn.usefathom.com/script.js" data-site="BDBWXXRL" defer data-included-domains="stake.hoprnet.org"></script>
         </Head>
         <body>
           <Main />
